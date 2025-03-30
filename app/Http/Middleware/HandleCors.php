010210d0ae99f504a4fd24c4aa0a10a19b1894da
Amin Hashemi <aaminhashemi74@gmail.com>
2025-03-30 10:34:53 +0330

    Implement cors middleware for api

---
+++ app/Http/Middleware/HandleCors.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class HandleCors
+{
+    public function handle(Request $request, Closure $next)
+    {
+        // Add CORS headers
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'https://todo-frontend-tmrg.onrender.com')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With'); // Allow specific headers
+    }
+}
