3cd026c756f622a2f2054715bdcdfed4117dd670
Amin Hashemi <aaminhashemi74@gmail.com>
2025-03-30 10:29:43 +0330

    Create cors.php

---
+++ config/cors.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS Options
+    |--------------------------------------------------------------------------
+    |
+    | Configure the options for CORS (Cross-Origin Resource Sharing) here.
+    | These options control what domains and methods are allowed in cross-origin
+    | requests.
+    |
+    */
+
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://todo-frontend-tmrg.onrender.com',
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false, 
+];
