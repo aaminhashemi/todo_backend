464a15bccaf31233989a66f3e0707a7f5373f2bb
Amin Hashemi <aaminhashemi74@gmail.com>
2025-03-31 11:44:52 +0330

    Update tests (conver to unit test)

--- tests/Unit/TodoTest.php
+++ tests/Unit/TodoTest.php
@@ -7,83 +7,33 @@ use Tests\TestCase;
 
 class TodoTest extends TestCase
 {
-    /** @test */
-    public function test_can_store_todo()
+    #[Test]
+    public function test_update_todo()
     {
-        // Data to be sent in the request (can be dynamic or predefined)
-        $data = [
-            'title' => 'Learn Laravel Testing',
-            'status' => 'pending',
-            'due' => '2025-09-14',
-        ];
-
-        // Send a POST request to save the new todo
-        $response = $this->postJson('https://127.0.0.1:8000/api/todo/create', $data);
-
-        // Assert that the response is successful (status code 201 - created)
-        $response->assertStatus(201);
-
-        // Assert that the response contains the correct data
-        $response->assertJson([
-            'data' => [
-                'title' => 'Learn Laravel Testing',
-                'status' => 'pending',
-                'due' => '2025-09-14',
-            ]
-        ]);
-
-        // Optionally, check if the data is saved in the database
-        $this->assertDatabaseHas('todos', [
-            'title' => 'Learn Laravel Testing',
-            'status' => 'pending',
-            'due' => '2025-09-14',
-        ]);
-    }
-
-    public function test_delete_todo_via_api()
-    {
-        // Create a todo to delete
-        $todo = Todo::create([
-            'title' => 'Test Todo',
-            'status' => 'in-progress',
-            'due' => '2025-09-25',
-        ]);
-
-        // Send a DELETE request to delete the todo
-        $response = $this->deleteJson("https://127.0.0.1:8000/api/todo/delete/?id={$todo->id}");
-
-        // Assert that the response is successful (status code 200 - OK)
+        $todo = Todo::factory()->create();
+        $possibleStatuses=['pending', 'completed', 'in-progress'];
+        $status=null;
+        foreach ($possibleStatuses as $item){
+            if($item!=$todo->status){
+                $status=$item;
+            }
+        }
+        $response = $this->postJson(route('api_update_todo', ['id'=>$todo->id,'status'=>$status]));
         $response->assertStatus(200);
-
-        // Assert that the response contains the correct message (if applicable)
-        $response->assertJson([
-            'message' => 'Todo deleted successfully.',
-        ]);
-
-        // Check if the todo is deleted from the database
-        $this->assertDatabaseMissing('todos', [
-            'id' => $todo->id,
-            'title' => 'Test Todo',
-        ]);
     }
-
-    public function test_get_data_from_api()
+    #[Test]
+    public function test_delete_todo()
     {
-        // Make a GET request to the API endpoint
-        $response = $this->get('https://127.0.0.1:8000/api/todo/list'); // Replace with your API URL
-
-        // Assert that the response is successful (status code 200)
+        $todo = Todo::factory()->create();
+        $response = $this->postJson(route('api_delete_todo', ['id'=>$todo->id]));
         $response->assertStatus(200);
-        // Optionally, check if the response contains the expected data
-        $response->assertJsonStructure([
-            'data' => [
-                '*' => [   // '*' indicates any item in the array
-                    'id',
-                    'title',
-                    'status',
-                    'due',
-                ]
-            ]
-        ]);
+        $this->assertDatabaseMissing('todos', ['id' => $todo->id]);
     }
+    #[Test]
+    public function test_get_todos()
+    {
+        $response = $this->getJson(route('api_list_todo'));
+        $response->assertStatus(200);
+    }
+
 }
